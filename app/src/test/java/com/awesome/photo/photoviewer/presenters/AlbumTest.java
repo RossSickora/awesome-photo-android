90ad2c1293fa2bc26d8c1db6bbbf9e42e8e5f59b
Ross Sickora <r.sickora@gmail.com>
2021-10-16 09:27:10 -0500

    cleaning up AlbumsTest unnecessary mock and unnecessary throws exception declaration

--- app/src/test/java/com/awesome/photo/photoviewer/presenters/AlbumTest.java
+++ app/src/test/java/com/awesome/photo/photoviewer/presenters/AlbumTest.java
@@ -5,9 +5,9 @@ import com.awesome.photo.remote.api.models.AlbumsModel;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -18,7 +18,7 @@ public class AlbumTest {
     List<AlbumsModel> albumsList;
     int baseId = 1;
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
         classUnderTest = new Album(baseId, albumsList);
     }
@@ -30,7 +30,7 @@ public class AlbumTest {
 
     @Test
     public void setPhotos() {
-        List<AlbumsModel> expected = Mockito.mock(List.class);
+        List<AlbumsModel> expected = new ArrayList<>();
         classUnderTest.setPhotos(expected);
         assertEquals(expected, classUnderTest.getPhotos());
     }
